2e9d2fe71043aa4ecddcfabf9e670a7fd97fa757
joannelu0712 <joannelu0712@gmail.com>
2023-12-11 20:19:03 +0800

    add lightbox

---
+++ js/func.js
@@ -0,0 +1,15 @@
+
+/*---------home button lightbox----------*/
+
+let lightbox_el = document.getElementsByClassName("lightbox")[0];
+let home_rel = document.getElementsByClassName("btn_blue")[0];
+home_rel.addEventListener("click", function () {
+    lightbox_el.classList.remove("none");
+});
+
+let remain_rel = document.getElementsByClassName("lightbox")[0].getElementsByClassName("btn_black")[0]
+
+remain_rel.addEventListener("click", function () {
+    console.log(lightbox_el);
+    lightbox_el.classList.add("none");
+});
