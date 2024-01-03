921eae12222f7a4a1f52548cad8da2f0ece38959
joannelu0712 <joannelu0712@gmail.com>
2024-01-03 22:18:19 +0800

    final

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ $(function () {
     let lightbox_el = document.getElementsByClassName("lightbox")[1];
     lightbox_el.addEventListener('click', function () {
         lightbox_el.classList.add('none');
-        window.location.assign('profile.html') //點擊視窗外強制前往會員中心
+        window.location.assign('../webPage/profile.html') //點擊視窗外強制前往會員中心
 
     })
     lightbox_el.querySelector('.popup').addEventListener('click', function (e) {
