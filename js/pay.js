2370e17c8eb76c8b81d48f0635d7da2e4e0d8042
joannelu0712 <joannelu0712@gmail.com>
2024-01-03 21:14:27 +0800

    fixed credit card input

--- js/pay.js
+++ js/pay.js
@@ -15,14 +15,20 @@ $(function () {
     let member = JSON.parse(localStorage.getItem("member"));
     // 非會員購票，只能信用卡or第三方支付
     if (member.is_member == false) {
+        // 因為非會員，要把會員票改掉
+        $('.ticket div:first-child h3').text('一般票')
+        $('.check section:nth-child(2) p:first-child').text('普通廳－一般票 * 1')
+        $('.check section:nth-child(2) p:last-child').text('$ 300')
+        $('.check h3:last-child').text('總計：$ 470')
+        $('.lightbox .container h3').html('取票條碼與取餐序號<br>已寄送至您的email信箱')
         $('.money a:nth-child(1)').removeClass('btn_on').addClass('btn_ban')
         $('.money a:nth-child(2)').removeClass('btn_off').addClass('btn_on')
         $('.money a:nth-child(3)').removeClass('btn_off').addClass('btn_off')
         $('.money a:nth-child(4)').removeClass('btn_off').addClass('btn_ban')
-
-        $('.cal').toggleClass('-off')
-        $('.card').toggleClass('-off')
+        $('.cal').addClass('-off')
+        $('.card').removeClass('-off')
     } else {// 會員購票，所有支付皆支援
+        $('#email').val('iamyourmember@gmail.com')
         $('.money a:nth-child(1)').click(function () {
             $('.cal').removeClass('-off')
             $('.card').addClass('-off')
